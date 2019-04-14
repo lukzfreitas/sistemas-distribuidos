de8bf371a5cb295b7f3d28b7b6e882c4c97219c7
lukz_afreitas <lucasrgs17@gmail.com>
2019-04-14 20:04:39 -0300

    primeiro commit

---
+++ P2P/src/cliente/PeerFileDownload.java
@@ -0,0 +1,83 @@
+package cliente;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class PeerFileDownload implements Runnable
+{
+	String fileDownloadPath;
+	int portNumber;
+	Socket peerSocket;
+	Scanner in;
+	PrintStream out;
+
+	public PeerFileDownload(String filePath,int userInpPort){
+		this.portNumber = userInpPort;
+		this.fileDownloadPath = filePath;
+	}
+
+	public void run(){
+		try{
+			ServerSocket downloadSocket = new ServerSocket(portNumber);
+			System.out.println("starting cliente socket now");
+			
+			while(true){
+				//accept the connection from the socket
+				peerSocket = downloadSocket.accept();
+				System.out.println("Cliente conectado para compartilhar arquivos ...");
+
+				out = new PrintStream(peerSocket.getOutputStream());
+				in = new Scanner(peerSocket.getInputStream());
+
+				//get the fileName from ClientAskingForFile
+				String fileName = in.nextLine();
+				System.out.println("Requested file is: "+fileName);
+				String peerForFile = in.nextLine();
+
+				File checkFile = new File(fileDownloadPath + "/" + fileName);
+				
+				FileInputStream fin = new FileInputStream(checkFile);
+				BufferedInputStream buffReader = new BufferedInputStream(fin);
+				
+				//check if the file exists, for it to be downloaded
+				if (!checkFile.exists()){
+					System.out.println("Arquivo não existe");
+					buffReader.close();
+					return;
+				}
+
+				//get the file size, as the buffer needs to be allocated an initial size
+				int size = (int) checkFile.length();	//convert from long to int
+				byte[] buffContent = new byte[size];
+				System.out.println("tamanho do arquivo" + size);
+				//send file size
+				out.println(size);
+				
+				//allocate a buffer to store contents of file
+				int startRead = 0;	//how much is read in total
+				int numOfRead = 0;	//how much is read in each read() call
+
+				//read into buffContent, from StartRead until end of file
+				while (startRead < buffContent.length && (numOfRead = buffReader.read(buffContent, startRead, buffContent.length - startRead)) >= 0) 
+				{
+					startRead = startRead + numOfRead;
+				}
+				//Validate all the bytes have been read
+				if (startRead < buffContent.length){
+					System.out.println("Leitura do arquivo incompleta" + checkFile.getName());
+				}
+				out.println(buffContent);
+				buffReader.close();
+			}
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+}
---
+++ P2P/src/server/Servidor.java
@@ -0,0 +1,147 @@
+package server;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.ListIterator;
+import java.util.Scanner;
+
+
+public class Servidor {
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket socket = new ServerSocket(5004);
+            System.out.println("Servidor conectado!!!");
+            while (true) {
+                Socket serverSocket = socket.accept();
+                Thread thread = new Thread(new PeerConnect(serverSocket));
+                thread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+class PeerConnect implements Runnable {
+
+    public static HashMap<String, ArrayList<String>> fileMap = new HashMap<String, ArrayList<String>>();
+
+    public Socket clientSocket;
+    int count = 0;
+
+    public PeerConnect(Socket socket) throws IOException {
+        this.clientSocket = socket;
+    }
+
+    @Override
+    public void run() {
+
+        Boolean condition = true;
+        String check = "";
+        Scanner sc = new Scanner(System.in);
+
+        while (condition) {
+            try {
+                String peerId, fileName, searchResult, error, checkId;
+                String option;
+
+                String location = "";
+                ArrayList<String> fileLocation = new ArrayList<String>();
+
+                Scanner serverIn = new Scanner(clientSocket.getInputStream());
+                PrintStream serverOut = new PrintStream(clientSocket.getOutputStream());
+
+                peerId = serverIn.nextLine();
+
+                option = serverIn.nextLine();
+                switch (option) {
+
+                    // Registrar arquivos
+                    case "1":
+                        File peerDirectory = new File(peerId);
+                        File[] sharedFiles = peerDirectory.listFiles();
+
+                        for (int i = 0; i < sharedFiles.length; i++) {
+                            registry(peerId, sharedFiles[i].getName());
+                        }
+                        serverOut.println(option);
+                        break;
+
+                    // Localizar arquivo
+                    case "2":
+                        fileName = serverIn.nextLine();
+                        fileLocation = lookup(fileName);
+                        try {
+                            for (int i = 0; i < fileLocation.size(); i++) {
+                                location += fileLocation.get(i) + " ";
+                            }
+                            searchResult = "O arquivo está presente na: " + location;
+                            System.out.println(location);
+                            serverOut.println(searchResult);
+
+                        } catch (Exception e) {
+                            error = "Arquivo não registrado";
+                            serverOut.println(error);
+                        }
+                        break;
+
+                    // Fazer download do arquivo
+                    case "3":
+                        serverOut.println(option);
+                        break;
+
+                    // Encerrar conexão
+                    case "4":
+                        System.out.println("Conexão encerrada!!!");
+                        condition = false;
+                        serverOut.println(option);
+                        break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    /*
+     * Register function - To register file present with each peer
+     */
+    public void registry(String peerId, String fileName) throws IOException {
+
+        ArrayList<String> peerList = new ArrayList<String>();
+        ArrayList<String> checkList = new ArrayList<String>();
+
+        peerList.add(peerId);
+        checkList = fileMap.get(fileName);
+
+        if (checkList == null || checkList.isEmpty()) {
+            fileMap.put(fileName, peerList);
+        } else {
+            for (int i = 0; i < checkList.size(); i++) {
+                if (checkList.get(i).equals(peerId)) {
+                    checkList.remove(i);
+                }
+            }
+            checkList.add(peerId);
+            fileMap.put(fileName, checkList);
+        }
+    }
+
+    public ArrayList<String> lookup(String fileName) throws IOException {
+
+        ArrayList<String> peerList = new ArrayList<String>();
+        peerList = fileMap.get(fileName);
+        return peerList;
+
+    }
+
+}
